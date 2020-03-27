5cc5307a2ee22b2e49fe0ad3d364c6634eb0c70f
espanet <yann.espanet@hotmail.com>
2020-03-27 13:57:30 +0100

    Add Storage page

---
+++ AwesomeWebApp/Controllers/ImageController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using AwesomeWebApp.Models;
+using System.Configuration;
+using BlobStorageDemo;
+
+namespace AwesomeWebApp.Controllers
+{
+    public class ImageController : Controller
+    {
+        ImageService imageService = new ImageService();
+
+
+        static CloudBlobClient blobClient;
+        const string blobContainerName = "webappstoragedotnet-imagecontainer";
+        static CloudBlobContainer CloudBlobContainer;
+
+
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                // Create a blob client for interacting with the blob service.
+                CloudStorageAccount cloudStorageAccount = ConnectionString.GetConnectionString();
+                CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+                CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference("sampleimage");
+
+                // To view the uploaded blob in a browser, you have two options. The first option is to use a Shared Access Signature (SAS) token to delegate  
+                // access to the resource. See the documentation links at the top for more information on SAS. The second approach is to set permissions  
+                // to allow public access to blobs in this container. Comment the line below to not use this approach and to use SAS. Then you can view the image  
+                // using: https://[InsertYourStorageAccountNameHere].blob.core.windows.net/webappstoragedotnet-imagecontainer/FileName 
+                await cloudBlobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+                // Gets all Cloud Block Blobs in the blobContainerName and passes them to teh view
+                List<Uri> allBlobs = new List<Uri>();
+                foreach (IListBlobItem blob in cloudBlobContainer.ListBlobs())
+                {
+                    if (blob.GetType() == typeof(CloudBlockBlob))
+                        allBlobs.Add(blob.Uri);
+                }
+
+                return View(allBlobs);
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+
+        // GET: Image  
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Upload(HttpPostedFileBase photo)
+        {
+            var imageUrl = await imageService.UploadImageAsync(photo);
+            TempData["LatestImage"] = imageUrl.ToString();
+            return RedirectToAction("LatestImage");
+        }
+
+        public ActionResult LatestImage()
+        {
+            var latestImage = string.Empty;
+            if (TempData["LatestImage"] != null)
+            {
+                ViewBag.LatestImage = Convert.ToString(TempData["LatestImage"]);
+            }
+
+            return View();
+        }
+
+
+
+
+        /// <summary> 
+        /// Task<ActionResult> DeleteImage(string name) 
+        /// Documentation References:  
+        /// - Delete Blobs: https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/#delete-blobs
+        /// </summary> 
+        [HttpPost]
+        public async Task<ActionResult> DeleteImage(string name)
+        {
+            try
+            {
+                CloudStorageAccount cloudStorageAccount = ConnectionString.GetConnectionString();
+                CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+                CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference("sampleimage");
+                Uri uri = new Uri(name);
+                string filename = Path.GetFileName(uri.LocalPath);
+
+                var blob = cloudBlobContainer.GetBlockBlobReference(filename);
+                await blob.DeleteIfExistsAsync();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+        /// <summary> 
+        /// Task<ActionResult> DeleteAll(string name) 
+        /// Documentation References:  
+        /// - Delete Blobs: https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/#delete-blobs
+        /// </summary> 
+        [HttpPost]
+        public async Task<ActionResult> DeleteAll()
+        {
+            try
+            {
+                CloudStorageAccount cloudStorageAccount = ConnectionString.GetConnectionString();
+                CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+                CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference("sampleimage");
+                foreach (var blob in cloudBlobContainer.ListBlobs())
+                {
+                    if (blob.GetType() == typeof(CloudBlockBlob))
+                    {
+                        await ((CloudBlockBlob)blob).DeleteIfExistsAsync();
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+
+    }
+}
